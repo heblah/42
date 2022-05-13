9c0128501d638ee94a23b3288dfc6ea69714e317
Hans ALVAREZ HERENCIA <halvarez@e3r8p16.clusters.42paris.fr>
2022-05-13 13:32:21 +0200

    13-05-2022

--- 02_ft_printf/ft_printf.c
+++ 02_ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: halvarez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 09:07:00 by halvarez          #+#    #+#             */
-/*   Updated: 2022/05/12 15:30:36 by halvarez         ###   ########.fr       */
+/*   Updated: 2022/05/13 13:32:11 by halvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,41 +16,29 @@ int	ft_printf(const char *s, ...)
 {
 	va_list argp;
 
-	argp = NULL;
+	va_start(argp, s);
 	while (*s)
 	{
 		if (*s != '%')
 			ft_putchar_fd(*s, 1);
 		else
-			if (is_conv(s))
-				s =  print_conv(s);
-			else if (is_flagged_conv(s))
-				s = print_flagged_conv(s);
-			else if (!is_conv(s) && !is_flagged_conv(s))
-				break;
+			(*print)(t_f, const char *, va_list)
+			print_with_conv(s, argp);
 		s++;
 	}
 }
 
-int	is_conv(char *sub_s)
+int	print_with_conv(const char *s, va_list argp)
 {
-	if (*(s + 1) && (*(s + 1) == 'c' || *(s + 1) == 's' || *(s + 1) == 'p'
-		|| *(s + 1) == 'd' || *(s + 1) == 'i' || *(s + 1) == 'u'
-		|| *(s + 1) == 'x' || *(s + 1) == 'X' || *(s + 1) == '%')
-		return (1);
-	return (0);
+	if (*(s + 1) &&
+		(*(s + 1) == 'c'))
+
 }
 
-int	is_flagged_conv(char *sub_s)
+int	init_flag(t_f flag)
 {
-	if (*(s + 1) && *(s + 1) == '#'
-		&& (*(s + 2) == 'x' || *(s + 2) == 'X'))
-		return (1);
-	else if (*(s + 1) && *(s + 1) == ' '
-		&& (*(s + 2) == 'd' || *(s + 2) == 'i') || *(s + 2) == 'u')
-		return (1);
-	else if (*(s + 1) && *(s + 1) == '+'
-		&& (*(s + 2) == 'd' || *(s + 2) == 'i' || *(s + 2) == 'u'))
-		return (1);
-	return (0);
+	(flag)->percent = 0;
+	(flag)->sharp = 0;
+	(flag)->blank = 0;
+	(flag)->plus = 0:
 }
--- 02_ft_printf/ft_printf.h
+++ 02_ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: halvarez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 09:04:13 by halvarez          #+#    #+#             */
-/*   Updated: 2022/05/12 18:06:32 by halvarez         ###   ########.fr       */
+/*   Updated: 2022/05/13 13:27:47 by halvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,12 @@
 /*structures de flags*/
 typedef struct	s_flag
 {
-	int	percent;
-	int	sharp;
-	int blank;
-	int	plus;
+	int	percent;/*suivi d'un %*/
+	int	sharp;/*suivi d4un x ou X*/
+	int blank;/*suivi d'un i, d ou u*/
+	int	plus;/*suivi d'un i, d ou u*/
 }		t_f;
+
 /*structures de conversions*/
 typedef struct	s_conv
 {
