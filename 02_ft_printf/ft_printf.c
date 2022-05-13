bfcebc11dca91c2758c0595f6e8f61ca2f7309e7
Hans ALVAREZ HERENCIA <halvarez@e3r10p18.clusters.42paris.fr>
2022-05-13 17:46:03 +0200

    13-05-2022

--- 02_ft_printf/ft_printf.c
+++ 02_ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: halvarez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 09:07:00 by halvarez          #+#    #+#             */
-/*   Updated: 2022/05/13 13:32:11 by halvarez         ###   ########.fr       */
+/*   Updated: 2022/05/13 17:45:22 by halvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,58 @@
 int	ft_printf(const char *s, ...)
 {
 	va_list argp;
+	int		(*print)(const char *, va_list);
 
 	va_start(argp, s);
 	while (*s)
 	{
 		if (*s != '%')
-			ft_putchar_fd(*s, 1);
+			write(1, s, 1);
+		else if (*s && *(s + 1) == '%')
+		{
+			write(1, "%", 1);
+			s++;
+		}
 		else
-			(*print)(t_f, const char *, va_list)
-			print_with_conv(s, argp);
+		{
+			print_selecter();
+			(*print)(s, argp)
+		}
 		s++;
 	}
 }
 
-int	print_with_conv(const char *s, va_list argp)
+int	init_flags(const char *s, t_f flag)
 {
-	if (*(s + 1) &&
-		(*(s + 1) == 'c'))
-
-}
-
-int	init_flag(t_f flag)
-{
-	(flag)->percent = 0;
 	(flag)->sharp = 0;
 	(flag)->blank = 0;
 	(flag)->plus = 0:
+
+	if (*s == '#')
+		(flag)->sharp = 1;
+	else if (*s == ' ')
+		(flag)->blank = 1;
+	else if (*s == '+')
+		(flag)->plus = 1;
+	else
+		return (1)
+	return (2);
+}
+
+int (*print_selecter(const char s, va_list argp))(const char *, va_list)
+{
+	t_f flag;
+	int	i;
+
+	i = init_flags(s, &flag);
+	if (*(s + i) && *(s + i) == 'c')
+		return (&ft_putchar_fd);
+	else if (*(s + i) && *(s + i) == 's')
+		return (&ft_putstr_fd);
+	else if (*(s + i) && (*(s + i) == 'd' || *(s + i) == 'i' || *(s + i) == 'u'))
+		return (&ft_putnbr_fd); //fonction a adapter pour les flags
+	else if (*(s + i) && (*(s + i) == 'p'))
+		return (&ft_putptr_fd); //fonction a adapter pour les flags
+	else if (*(s + i) && (*(s + i) == 'x' || *(s + i) == 'X')
+		return (&ft_puthexa_fd); //fonction a adapter pour les flags
 }
