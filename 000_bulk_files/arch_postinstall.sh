9c5cef1f16e3a1a2456aff75a5058927a8710e5b
Hans Alvarez Herencia <hansalvarezherencia@protonmail.com>
2022-06-20 21:25:37 +0200

    20-06-2022 21h25

--- 000_bulk_files/arch_postinstall.sh
+++ 000_bulk_files/arch_postinstall.sh
@@ -6,7 +6,7 @@
 #    By: halvarez <halvarez@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/06/15 14:08:50 by halvarez          #+#    #+#              #
-#    Updated: 2022/06/16 07:33:57 by halvarez         ###   ########.fr        #
+#    Updated: 2022/06/20 17:13:48 by halvarez         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -32,12 +32,20 @@
  systemctl mask systemd-rfkill.socket
  systemctl enable tlp.service
  systemctl restart tlp.service
+ 
+#if sound not recognized
+ pacman -S sof-firmware alsa-ucm-conf
 
-#git clone 42 repo
+#42 stuff
  git clone git@github.com:heblah/42.git /home/hans/42
  cp home/hans/42/.vimrc home/hans/.
- git clone https://github.com/42Paris/42header.git /home/hans/
+ git clone https://github.com/43Paris/42header.git /home/hans/header
  mkdir -p /home/hans/.vim
+ cp -r /home/hans/header/plugin /home/hans/.vim/.
+ sudo pacman -S pyhton python-pip
+ git clone https://github.com/42School/norminette.git /home/hans/norminette
+ python3 -m pip install --upgrade pip setuptools
+ python3 -m pip install norminette
 
 #rebooting
  sleep 10 && systemctl reboot
