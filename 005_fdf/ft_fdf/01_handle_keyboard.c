e14a4bb2d4e4b5357cd745073eacd955fba559c7
Hans Alvarez Herencia <hansalvarezherencia@protonmail.com>
2022-07-07 10:47:03 +0200

    07-07-2022 10h47

--- 005_fdf/ft_fdf/01_handle_keyboard.c
+++ 005_fdf/ft_fdf/01_handle_keyboard.c
@@ -6,7 +6,7 @@
 /*   By: halvarez <halvarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 12:02:52 by halvarez          #+#    #+#             */
-/*   Updated: 2022/07/04 17:37:17 by halvarez         ###   ########.fr       */
+/*   Updated: 2022/07/07 09:39:27 by halvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,10 @@ int manage_keyboard(t_data *data)
 {
 	//mlx_loop_hook(data.mlx_ptr, &handle_no_event, &data);
 	mlx_hook(data->win_ptr, KeyPress, KeyPressMask, &handle_keypress, data);
-//	mlx_hook(data->win, ClientMessage, LeaveWindowMask, \
-//		&cross_btn_handler, data);
+	/*
+	mlx_hook(data->win, ClientMessage, LeaveWindowMask, \
+		&cross_btn_handler, data);
+	*/
 	mlx_loop(data->mlx_ptr);
 	return (0);
 }
