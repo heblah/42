e968e28b4a41fcea5beeb6f470f99bb989c4c3f3
Hans <hansalvarezherencia@protonmail.com>
2022-08-11 16:35:15 +0200

    test

--- 006_push_swap/srcs/01_list_utils.c
+++ 006_push_swap/srcs/01_list_utils.c
@@ -6,7 +6,7 @@
 /*   By: halvarez <halvarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 10:27:07 by halvarez          #+#    #+#             */
-/*   Updated: 2022/08/11 16:34:54 by halvarez         ###   ########.fr       */
+/*   Updated: 2022/08/11 16:35:09 by halvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,4 +108,3 @@ void	mv_front(t_lst **first_a, t_lst **first_b)
 		(*first_b)->next = tmp;
 	}
 }
-lkj
