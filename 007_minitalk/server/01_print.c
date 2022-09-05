56bdec5b96cd1ae1e7c17680d82af504ba3c526d
Hans Alvarez Herencia <hansalvarezherencia@protonmail.com>
2022-09-05 17:30:08 +0200

    007 starting minitalk

--- 007_minitalk/server/01_print.c
+++ 007_minitalk/server/01_print.c
@@ -1,34 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   unicode_tests.c                                    :+:      :+:    :+:   */
+/*   01_print.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: halvarez <halvarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/03 11:48:17 by halvarez          #+#    #+#             */
-/*   Updated: 2022/09/05 17:18:14 by halvarez         ###   ########.fr       */
+/*   Created: 2022/09/05 17:26:04 by halvarez          #+#    #+#             */
+/*   Updated: 2022/09/05 17:29:24 by halvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <signal.h>
 
-int	main(void)
+void	printpid(pid_t pid)
 {
-	unsigned char	utf[3];
-
-	utf[0] = 65; //only byte of A
-	utf[1] = 195; //=11000011 first byte of é
-	utf[2] = 169; //=10101001 second byte of é
-	write(1, utf, 3);
-	write(1, "\n", 1);
-
-	write(1, utf, 1);
-	write(1, utf + 1, 1);
-	write(1, utf + 2, 1);
-	write(1, "\n", 1);
-
-	write(1, utf, 1);
-	write(1, utf + 1, 2);
-	write(1, "\n", 1);
-	return (0);
+	if (pid > 9)
+		printpid(pid / 10);
+	pid += '0';
+	write(1, &pid, 1);
 }
