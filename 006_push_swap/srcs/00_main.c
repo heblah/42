a86545e60463aee94328cdb8b09b9c07a6ed2f48
Hans Alvarez Herencia <hansalvarezherencia@protonmail.com>
2022-09-09 16:51:53 +0200

    008 philo Makefile

--- 006_push_swap/srcs/00_main.c
+++ 006_push_swap/srcs/00_main.c
@@ -6,7 +6,7 @@
 /*   By: halvarez <halvarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 10:01:15 by halvarez          #+#    #+#             */
-/*   Updated: 2022/09/02 13:00:59 by halvarez         ###   ########.fr       */
+/*   Updated: 2022/09/09 15:46:52 by halvarez         ###   ########.fr       */
 /*   Updated: 2022/08/25 17:11:57 by halvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -70,7 +70,7 @@ int	main(int argc, char **argv)
 	{
 		stack = parser((const int)argc, (const char **)argv);
 		sort(&stack, a);
-		//print_stack(stack);
+		print_stack(stack);
 		ft_printf("stack a is sorted = %d\t\t", is_sorted(&stack, a));
 		ft_printf("count=%d\n", stack->count);
 		//ft_printf("size=%d\n", stack->a_size);
