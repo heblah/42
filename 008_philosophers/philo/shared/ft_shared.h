cd49cd7de1f84716ce5a001ab57c70db7502eb57
halvarez <hansalvarezherencia@protonmail.com>
2022-11-17 14:43:35 +0100

    008 shared files

--- 008_philosophers/philo/shared/ft_shared.h
+++ 008_philosophers/philo/shared/ft_shared.h
@@ -1,5 +1,17 @@
-#ifndef SHARED_H
-# define SHARED_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_shared.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: halvarez <halvarez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 14:43:03 by halvarez          #+#    #+#             */
+/*   Updated: 2022/11/17 14:43:16 by halvarez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_SHARED_H
+# define FT_SHARED_H
 
 # include <stdlib.h>
 # include <stdio.h>
