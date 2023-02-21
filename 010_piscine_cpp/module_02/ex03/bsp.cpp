ebb9ef72e6001e5d20f1fd2d6c6698e11adbc51a
Hans Alvarez Herencia <hansalvarezherencia@protonmail.com>
2023-02-21 17:01:29 +0100

    cpp/02/03

--- 010_piscine_cpp/module_02/ex03/bsp.cpp
+++ 010_piscine_cpp/module_02/ex03/bsp.cpp
@@ -1,35 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   bsp.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: halvarez <halvarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/20 09:17:32 by halvarez          #+#    #+#             */
-/*   Updated: 2023/02/21 16:42:08 by halvarez         ###   ########.fr       */
+/*   Created: 2023/02/21 16:58:59 by halvarez          #+#    #+#             */
+/*   Updated: 2023/02/21 16:59:55 by halvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 #include "Fixed.hpp"
-
-int	main( void )
-{
-	Fixed	a(-1000);
-	Fixed const	b( Fixed( 5.05f ) * Fixed( 2 ) );
-
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-
-	std::cout << b << std::endl;
-	
-	--a;
-	--a;
-	std::cout << Fixed::max( a, b ) << std::endl;
-	std::cout << Fixed::min( a, b ) << std::endl;
-	return 0;
-}
