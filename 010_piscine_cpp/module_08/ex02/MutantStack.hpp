3aa791c6231a04a53c435de5f614ad1cf2fa5d25
halvarez <hansalvarezherencia@protonmail.com>
2023-03-09 22:13:24 +0100

    010/m08/ex02 wip

---
+++ 010_piscine_cpp/module_08/ex02/MutantStack.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: halvarez <halvarez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/09 22:08:07 by halvarez          #+#    #+#             */
+/*   Updated: 2023/03/09 22:13:08 by halvarez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP 
+
+class MutantStack
+{
+	public:
+
+	private:
+
+};
+
+#endif /* MUTANTSTACK_HPP */
