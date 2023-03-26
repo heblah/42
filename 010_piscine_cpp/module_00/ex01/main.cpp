630f8209fd81368a4952e7192df017d278f0d139
halvarez <hansalvarezherencia@protonmail.com>
2023-03-26 15:40:09 +0200

    010/00/ex01 check for eof

--- 010_piscine_cpp/module_00/ex01/main.cpp
+++ 010_piscine_cpp/module_00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: halvarez <halvarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 12:58:37 by halvarez          #+#    #+#             */
-/*   Updated: 2023/02/11 16:13:06 by halvarez         ###   ########.fr       */
+/*   Updated: 2023/03/26 15:39:40 by halvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ int	main(void)
 	{
 		std::cout << std::endl << "Please select an option : ADD, SEARCH or EXIT : ";
 		std::getline(std::cin, input);
+		if ( std::cin.eof() )
+			return (1);
 		if (input.compare("ADD") == 0)
 			book.add();
 		else if (input.compare("SEARCH") == 0)
